827afb28ae84abb2d651a1bb9b3a59200c69576b
François KLINGLER <francois@fklingler.com>
2010-09-16 19:59:08 +0200

    Déplacement du Logger dans Libs, modification du TODO, et application du tag 0.5

--- 42framework/libs/Logger.php
+++ 42framework/libs/Logger.php
@@ -16,7 +16,7 @@
  * License along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
-namespace Framework;
+namespace Framework\Libs;
 defined('FRAMEWORK_DIR') or die('Invalid script access');
 
 class LoggerException extends Exception { }
