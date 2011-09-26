60f203d428f5975ca39f70ad1cc9e9ec89a32083
Kevin O'NEILL <contact@kevinoneill.fr>
2011-09-26 20:59:23 +0200

    modif website/index

--- framework/modules/website/views/index.php
+++ framework/modules/website/views/index.php
@@ -1,10 +1,1 @@
-<?php 
-
-/* @var $cache \framework\libs\cache\MemcacheEngine */
-$cache = $this->getComponent('cache', 'Memcache');
-$cache->write('eerre', 'Memcache fonctionne :)');
-echo $cache->read('eerre');
-$cache->clear(false);
-echo $cache->read('eerre');
-
-?>
+Hello world !
