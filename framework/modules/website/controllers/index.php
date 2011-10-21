5693ef411f2ef40457f321b6af07a987ba296607
Kevin O'NEILL <contact@kevinoneill.fr>
2011-10-21 13:57:03 +0200

    Modification de website/index

--- framework/modules/website/controllers/index.php
+++ framework/modules/website/controllers/index.php
@@ -12,9 +12,7 @@ class Index extends \framework\core\Controller
 
 	public function processAction ()
 	{
-		$model = $this->getComponent('gacela')->loadMapper('House')->find(1);
-		echo '<pre>';
-		\var_dump($model);
+		
 	}
 
 	public function _after ()
