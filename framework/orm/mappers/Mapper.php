5d652d6d407bb457ea7d73e407ca53e3ef9d1313
Mickael GOETZ <mickael.goetz@gmail.com>
2012-05-13 18:44:28 +0200

    TODO Mapper

--- framework/orm/mappers/Mapper.php
+++ framework/orm/mappers/Mapper.php
@@ -18,6 +18,8 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
+//TODO: add 'read-only' attribute in fields definition
+
 namespace framework\orm\mappers;
 
 class MapperException extends \Exception
